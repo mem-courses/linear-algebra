085585662a257040e75e5fba69480cccf5ef0f69
Memento mori <memset0@outlook.com>
2023-09-19 09:50:26 +0800

    autosync

--- format.py
+++ format.py
@@ -1,2 +1,11 @@
+import os
+from os import path
+
 mappings = [("。", "．")]
 
+dirname = path.abspath(path.dirname(__file__))
+
+for current, dirs, files in os.walk(dirname):
+    for file in files:
+        if path.extname(file) == '.typ':
+            print(current, file)
